05282a0d5b177c1bdb57743cb32bf93c20b24140
unbiqo <damir.sarsenov12@gmail.com>
2025-06-08 17:46:39 -0700

    complete 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let finalString = '';
+    for (let n = 0; n < num; n++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
