9a09a33e72a09b878b4f46450ce5eebd2549204d
unbiqo <damir.sarsenov12@gmail.com>
2025-06-09 10:05:21 -0700

    complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let total = 0;
 
+    if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)
+        || !(typeof(min) === "number") || !(typeof(max) === "number")
+    ) {
+        return 'ERROR';
+    }
+    if (min > max) {
+        let temp = 0;
+        temp = min;
+        min = max;
+        max = temp;
+    }
+    for (i = min; i < max + 1; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
