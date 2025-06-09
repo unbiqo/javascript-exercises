d63f25d85684e03ab472deaa2965ffdc56f3be7d
unbiqo <damir.sarsenov12@gmail.com>
2025-06-08 18:22:44 -0700

    complete 08_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let list = string.split('');
+    let result = '';
+    for (i = list.length - 1; i >= 0; i--){
+        result += list[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
