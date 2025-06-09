ee5b738dac5b33fc172dc6821e38e3678fc408ec
unbiqo <damir.sarsenov12@gmail.com>
2025-06-08 18:31:24 -0700

    refactor: optimize 07_reverseString implementation using built-in methods

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function(string) {
-    let list = string.split('');
-    let result = '';
-    for (i = list.length - 1; i >= 0; i--){
-        result += list[i];
-    }
-    return result;
+    return Array.from(string).reverse().join("");
 };
 
 // Do not edit below this line
