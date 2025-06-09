486d9ad1c2061db3a57890c3f694ec2ff606eb3b
unbiqo <damir.sarsenov12@gmail.com>
2025-06-09 11:27:20 -0700

    compelte 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    // racecar
+    string = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('')
+
+    let reverseString = string.split('').reverse().join('');
+    return string === reverseString;
+    /*
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseString += string.at(i);
+    }
+    if (reverseString === string) {
+        return true;
+    }
+    else {
+        return false;
+    } */
 };
 
 // Do not edit below this line
