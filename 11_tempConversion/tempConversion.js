548106b4a2c6e99c3a45e8a92619114ee170a722
unbiqo <damir.sarsenov12@gmail.com>
2025-06-09 10:22:47 -0700

    complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let formula = (x - 32) * 5 / 9;
+  return Math.round(formula * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let formula = x * 9 / 5 + 32;
+  return Math.round(formula * 10) / 10;
 };
 
 // Do not edit below this line
