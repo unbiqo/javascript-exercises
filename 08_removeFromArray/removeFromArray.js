a62cbf8f368b5b286cda7f0af55a8072ce6edda3
unbiqo <damir.sarsenov12@gmail.com>
2025-06-09 09:49:15 -0700

    complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
