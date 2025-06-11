1991c8a371c2f7d22a8f6667dee56456eb965d0f
unbiqo <damir.sarsenov12@gmail.com>
2025-06-10 19:51:53 -0700

    complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const getAge = function(yearOfBirth, yearOfDeath) {
+        if (yearOfDeath) {
+            return yearOfDeath - yearOfBirth;
+        }
+        else {
+            return new Date().getFullYear() - yearOfBirth;
+        }
+    }
 
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        //const oldestAge = (oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth);
+        
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        //const currentAge = (current.yearOfDeath ? current.yearOfDeath - current.yearOfBirth : new Date().getFullYear() - current.yearOfBirth);
+        return oldestAge < currentAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
